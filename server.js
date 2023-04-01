724ec2f4e95239d6d71c137259ce8e5d1ec25ada
dumanmonster <dybysbayd@gmail.com>
2023-04-02 03:25:04 +0600

    initial

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/api/items", (req, res) => {
+  try {
+    const data = fs.readFileSync("./items.json", "utf8");
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.put('/api/items', (req, res) => {
+  try {
+    const newData = req.body;
+    fs.writeFileSync('./items.json', JSON.stringify(newData));
+    res.send('Data updated successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+app.get("/api/deals", (req, res) => {
+  try {
+    const data = fs.readFileSync("./deals.json", "utf8");
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+app.post('/api/deals', (req, res) => {
+  try {
+    const data = fs.readFileSync('./deals.json', 'utf8');
+    const deals = JSON.parse(data);
+    const dealId = uuidv4(); 
+    const newItem = req.body;
+    deals.push({dealId: dealId, ...newItem});
+    fs.writeFileSync('./deals.json', JSON.stringify(deals));
+    res.send('Deal added successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+app.put('/api/deals', (req, res) => {
+  try {
+    const newData = req.body;
+    fs.writeFileSync('./deals.json', JSON.stringify(newData));
+    res.send('Deals updated successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
